8c5ca018f90de526c0cc72b54aa3ea03020df719
Nishil_Agrawal <nanishil5@gmail.com>
2023-04-26 14:04:08 -0400

    Modified stats2.py

--- stats2.py
+++ stats2.py
@@ -28,8 +28,10 @@ output_folder = "output_stats"
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
+print("Processing files...")
 # Find all CSV files starting with 'rangeQTime'
 for i_filename in glob.glob('rangeQTime*.csv'):
+    print("Processing file: ", i_filename)
     # Read float values from the file
     with open(i_filename, 'r') as file:
         reader = csv.reader(file)
@@ -46,7 +48,7 @@ for i_filename in glob.glob('rangeQTime*.csv'):
         writer = csv.writer(file)
         writer.writerow(['Mean', 'Median', 'Min', 'Max', 'Std', 'Var', '95th', '99th'])
         writer.writerow(stats)
-
+print("Making graphs...")
 # Find all CSV files with "_stats" in the name
 for false_stats_file in glob.glob(os.path.join(output_folder, '*rc_off_False*_stats.csv')):
     true_stats_file = false_stats_file.replace("rc_off_False", "rc_off_True")
@@ -67,7 +69,7 @@ for false_stats_file in glob.glob(os.path.join(output_folder, '*rc_off_False*_st
         rects2 = ax.bar(x + width / 2, true_stats, width, label='Vanilla')
 
         # Add labels, title and custom x-axis tick labels
-        ax.set_ylabel('Values')
+        ax.set_ylabel('Time (Seconds)')
         ax.set_title('Comparison of Statistics for RangeDrivenCompaction and Vanilla')
         ax.set_xticks(x)
         ax.set_xticklabels(headers)
